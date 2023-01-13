e29b8a0dcc55b77fcb0323bf0e8fdd5f5b3cad79
pokavv <okaps@hanmail.net>
2023-01-13 11:58:55 +0900

    230113 track_iss project

---
+++ track_iss/practice.py
@@ -0,0 +1,18 @@
+'''
+1xx: Hold on
+2xx: Here you go
+3xx: Go away (can't access)
+4xx: You screwed up
+'''
+
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+latitude = data['iss_position']['latitude']
+longitude = data['iss_position']['longitude']
+iss_position = (latitude, longitude)
+
+print(iss_position)
