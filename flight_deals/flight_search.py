e0fb70479df5ade165e65c69d3458796deb70ec2
pokavv <okaps@hanmail.net>
2023-02-20 23:59:41 +0900

    230220 flight_deals project updated

--- flight_deals/flight_search.py
+++ flight_deals/flight_search.py
@@ -1,3 +1,16 @@
+import requests
+
+TEQUILA_ENDPOINT = 'https://tequila-api.kiwi.com'
+TEQUILA_API_KEY = 'API KEY'
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    
+    def get_destination_code(self, city_name):
+        loc_endpoint = f'{TEQUILA_ENDPOINT}/locations/query'
+        headers = {'apikey': TEQUILA_API_KEY}
+        query = {'term': city_name, 'location_types': 'city'}
+        response = requests.get(url=loc_endpoint, headers=headers, params=query)
+        result = response.json()['locations']
+        code = result[0]['code']
+        
+        return code
